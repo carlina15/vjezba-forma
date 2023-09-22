94ec8f6555b0b6fbee9da68b819eb138ce2c32c0
Karla Vujevic <karla_vu@icloud.com>
2023-09-22 18:39:05 +0200

    vjezba forme

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import './App.css';
+import { UserFunction } from './user';
+import UserForm from './user/UserForm';
+
+function App() {
+  const initialUsers = [
+    { id: 1, name: 'Ivan', years: 30 },
+    { id: 2, name: 'Marko', years: 35 },
+    { id: 3, name: 'Ana', years: 25 },
+  ];
+
+  const [users, setUsers] = useState(initialUsers);
+
+  const uvecajGodine = () => {
+    setUsers((prevState) => {
+      const updatedUsers = prevState.map((user) => ({
+        ...user,
+        years: user.years + 1,
+      }));
+      return updatedUsers;
+    });
+  };
+
+  const handleChangeName = (event, index) => {
+    const newUsers = [...users];
+    newUsers[index].name = event.target.value;
+    setUsers(newUsers);
+  };
+
+  const addUser = (newUser) => {
+    setUsers((prevState) => [...prevState, { ...newUser, id: prevState.length + 1 }]);
+  };
+
+  return (
+    <>
+      <h1>State</h1>
+      {users.map((user, index) => (
+        <UserFunction
+          key={user.id}
+          name={user.name}
+          years={user.years}
+          changeName={(event) => handleChangeName(event, index)}
+        />
+      ))}
+      <div>
+        <button onClick={uvecajGodine}>Uvecaj godine</button>
+      </div>
+      <UserForm addUser={addUser} />
+    </>
+  );
+}
+
+export default App;
---
+++ src/user/UserForm.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+
+const UserForm = ({ addUser }) => {
+  const [newUser, setNewUser] = useState({ name: '', years: '' });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setNewUser({ ...newUser, [name]: value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    addUser(newUser);
+    setNewUser({ name: '', years: '' });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Ime:
+        <input
+          type="text"
+          name="name"
+          value={newUser.name}
+          onChange={handleInputChange}
+        />
+      </label>
+      <label>
+        Godine:
+        <input
+          type="text"
+          name="years"
+          value={newUser.years}
+          onChange={handleInputChange}
+        />
+      </label>
+      <button type="submit">Dodaj korisnika</button>
+    </form>
+  );
+};
+
+export default UserForm;
